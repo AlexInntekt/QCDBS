a8714532f711089cfeef30d1ae0f244fd37bb7cc
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-08-19 21:00:00 +0300

    1st commit. Getting the things ready for work

---
+++ QuickAccessDataBaseScrapping/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QuickAccessDataBaseScrapping
+//
+//  Created by Manolescu Mihai Alexandru on 19/08/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
